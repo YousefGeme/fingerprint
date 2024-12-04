09509d3d52d4d945046487969fb83418e622c170
Yousef Gamel <elgamelyousef6@gmail.com>
2024-12-04 19:28:51 +0200

    finish

--- script.js
+++ script.js
@@ -47,3 +47,56 @@ window.addEventListener("scroll", function () {
     parallaxDi.style.transform = `translateY(${scrolly * .2}px)`;
 });
 
+// project swiper
+var swiper = new Swiper(".mmySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 5
+        },
+        640: {
+            slidesPerView: 1.5,
+            spaceBetween: 5
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        }
+    }
+});
+
+// counter
+let container = document.querySelector(".container");
+let counter = document.querySelectorAll(".card-foot .counter");
+let start = false;
+window.onscroll = function () {
+    if (window.scrollY >= container.offsetTop) {
+        if (!start) {
+            counter.forEach((num) => startCount(num));
+        }
+        start = true
+    }
+};
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 3000 / goal);
+}
